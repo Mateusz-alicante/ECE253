063a68fa6af787a0cc765c6af84114067c5d73b8
Mateusz Kazimierczak <mateuszkazimierczak@Mateuszs-MacBook-Pro.local>
2023-10-16 16:36:09 -0400

    fix

--- 3/adder.sv
+++ 3/adder.sv
@@ -11,7 +11,7 @@ endmodule
 
 
 module part1(input logic [3:0] a, b, input logic c_in,
-output logic [3:0] s, c_out);
+output logic [3:0] s, output logic c_out);
     logic c1, c2, c3;
 
     full_adder u1(a[0],b[0],c_in,    s[0],c1);
