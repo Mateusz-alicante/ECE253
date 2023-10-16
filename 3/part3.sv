1db4f964cb205cf73cfbf6eb59ce71b502d3d4ee
Mateusz Kazimierczak <mateuszkazimierczak@Mateuszs-MacBook-Pro.local>
2023-10-16 17:29:31 -0400

    fix

--- 3/part3.sv
+++ 3/part3.sv
@@ -1,15 +1,15 @@
 module part3(A, B, Function, ALUout)
     parameter N = 4;
-    input logic [N-1:0] a, b;
+    input logic [N-1:0] A, B;
     input logic [1:0] Function;
     output logic [( 2 * N )-1:0] ALUout;
 
     always_comb
     case (f)
-        2'b00: ALUout = a + b;
-        2'b01: if ((|a) | (|b)) ALUout = 1;
-        2'b10: if ((&a) & (&b)) ALUout = 1;
-        2'b11: ALUout = {a, b};
+        2'b00: ALUout = A + B;
+        2'b01: if ((|A) | (|B)) ALUout = 1;
+        2'b10: if ((&A) & (&B)) ALUout = 1;
+        2'b11: ALUout = {A, B};
     default : ALUout = 0;
     endcase
 
