0b810401f270df3ce0cc489a1b6c7ad0328931c1
wernerweinhartner <50850851+wernerweinhartner@users.noreply.github.com>
2023-10-16 17:10:17 -0400

    Update part2.sv

--- 3/part2.sv
+++ 3/part2.sv
@@ -39,3 +39,82 @@ module top(LEDR, SW, KEY, HEX0, HEX1, HEX2, HEX3);
     alu ualu(SW[7:4], SW[3:0], KEY[1:0], out)
 endmodule
 
+module full_adder(input logic a, b, c_in, output logic s, c_out)
+    logic y;
+    assign y a^b;
+    assign s y^c_in;
+
+    always_comb
+    c_out = y ? c_in : b;
+
+endmodule
+
+
+module part1(input logic [3:0] a, b, input logic c_in,
+output logic [3:0] s, c_out);
+    logic c1, c2, c3;
+
+    full_adder u1(a[0],b[0],c_in,    s[0],c1);
+    full_adder u2(a[1],b[1],c1,      s[1],c2);
+    full_adder u3(a[2],b[2],c2,      s[2],c3);
+    full_adder u4(a[3],b[3],c3,      s[3],c_out);
+
+endmodule
+
+
+module hex_decoder(input logic [3:0] c, output logic [6:0] display);
+
+    // (c[0] & c[1] & c[2] & c[3] ) |
+
+    assign display[0] = 
+        (~c[3] & ~c[2] & ~c[1] & c[0]) | 
+        (~c[3] & c[2] & ~c[1] & ~c[0] ) | 
+        (c[3] & ~c[2] & c[1] & c[0] ) | 
+        (c[3] & c[2] & ~c[1] & c[0] );
+
+    assign display[1] = 
+        (~c[3] & c[2] & ~c[1] & c[0]) | 
+        (~c[3] & c[2] & c[1] & ~c[0] ) | 
+        (c[3] & ~c[2] & c[1] & c[0] ) | 
+        (c[3] & c[2] & ~c[1] & ~c[0] ) |
+        (c[3] & c[2] & c[1] & ~c[0] ) |
+        (c[3] & c[2] & c[1] & c[0] );
+
+    assign display[2] =
+        (~c[3] & ~c[2] & c[1] & ~c[0]) | 
+        (c[3] & c[2] & ~c[1] & ~c[0] ) | 
+        (c[3] & c[2] & c[1] & ~c[0] ) | 
+        (c[3] & c[2] & c[1] & c[0] );
+
+    assign display[3] =
+        (~c[3] & ~c[2] & ~c[1] & c[0]) | 
+        (~c[3] & c[2] & ~c[1] & ~c[0] ) | 
+        (~c[3] & c[2] & c[1] & c[0] ) | 
+        (c[3] & ~c[2] & ~c[1] & c[0] ) |
+        (c[3] & ~c[2] & c[1] & ~c[0] ) |
+        (c[3] & c[2] & c[1] & c[0] );
+
+    assign display[4] = 
+        (~c[3] & ~c[2] & ~c[1] & c[0]) | 
+        (~c[3] & ~c[2] & c[1] & c[0]) | 
+        (~c[3] & c[2] & ~c[1] & ~c[0] ) | 
+        (~c[3] & c[2] & ~c[1] & c[0] ) | 
+        (~c[3] & c[2] & c[1] & c[0] ) | 
+        (c[3] & ~c[2] & ~c[1] & c[0] );
+
+    assign display[5] = 
+        (~c[3] & ~c[2] & ~c[1] & c[0]) | 
+        (~c[3] & ~c[2] & c[1] & ~c[0]) | 
+        (~c[3] & ~c[2] & c[1] & c[0]) | 
+        (~c[3] & c[2] & c[1] & c[0]) | 
+        (c[3] & ~c[2] & ~c[1] & c[0]);
+
+    assign display[6] = 
+        (~c[3] & ~c[2] & ~c[1] & ~c[0]) | 
+        (~c[3] & ~c[2] & ~c[1] & c[0]) | 
+        (~c[3] & c[2] & c[1] & c[0]) | 
+        (c[3] & c[2] & ~c[1] & ~c[0]);
+
+
+
+endmodule
